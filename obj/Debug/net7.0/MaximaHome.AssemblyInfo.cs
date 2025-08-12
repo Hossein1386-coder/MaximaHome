af048e1bcdb8ff31e95527fd4cd25084a9ed3c9e
Hossein1386-coder <hosseinmirzahasani3@gmail.com>
2025-08-12 06:47:13 -0700

    jj

--- obj/Debug/net7.0/MaximaHome.AssemblyInfo.cs
+++ obj/Debug/net7.0/MaximaHome.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MaximaHome")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23453fc34443d277d577c712e437dd661f783416")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbfa99d25504fb16bd2e309728edba6b11f93e6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MaximaHome")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MaximaHome")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
